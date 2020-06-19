6d49b4727f72910460b6383a612cb2b5af6d9c03
Gabriele <gabrielepese@gmail.com>
2020-06-19 17:31:50 +0200

    x andre

--- JSNACK 5/script.js
+++ JSNACK 5/script.js
@@ -1,13 +1,13 @@
 // Nella pagina ci sono due input(num1, num2) e una select con options(somma,sottrai,moltiplica,dividi).
-// // Al click su un bottone, il programma inserisce in un p#result il risultato dell'operazione scelta.
+// // Al click su un bottone, il programma inserisce in un p#risultato il risultato dell'operazione scelta.
 
 document.getElementById("button").addEventListener("click", function(){
   var num1 = document.getElementById("num1").value;
   var num1 = document.getElementById("num2").value;
+  var risultato = document.getElementById("risultato");
 
-  if (document.getElementById("opt").value == 1) {console.log("scelto 1")}
-  else if (document.getElementById("opt").value == 2) {console.log("scelto 2")
-
-  }
-
+  if (document.getElementById("opt").value == 1) {risultato.innerHTML = num1 + num2}
+  else if (document.getElementById("opt").value == 2) {risultato.innerHTML = num1 - num2}
+  else if (document.getElementById("opt").value == 3) {risultato.innerHTML = num1 * num2}
+  else if (document.getElementById("opt").value == 4) {risultato.innerHTML = num1 / num2}
 });
